c71305026ee05b31be3de5b33c28020b0edf9e84
Georg Tirpitz <geotir@gmx.de>
2025-06-20 11:38:37 +0200

    set ablation args

--- models/NGBoost_main.py
+++ models/NGBoost_main.py
@@ -4,12 +4,14 @@ import os
 os.environ["PYTHONWARNINGS"] = "ignore"
 from RegressionsModels import NGBoostRegressionModel
 import pandas as pd
-from evidential_boost import NormalInverseGamma, NIGLogScore
+from faster_evidential_boost import NormalInverseGamma, NIGLogScore
 from ngboost.distns.normal import Normal, NormalCRPScore, NormalLogScore
 from sklearn.tree import DecisionTreeRegressor
 from matplotlib import pyplot as plt
 import numpy as np
 import logging
+from xgboost import XGBRegressor
+
 
 
 #from sklearn.datasets import load_diabetes
@@ -52,19 +54,22 @@ def main(folder_path, data_path, target, identifier, out, folds=10):
     
 
     param_grid_ngb = {
-    'n_estimators': [300, 400, 500, 600],
-    'learning_rate': [0.05, 0.1],
-    'Base__max_depth': [3, 4]
-    }   
+    'n_estimators': [450, 500, 550, 600, 650],
+    'learning_rate': [0.5, 0.01, 0.1],
+    'Base__max_depth': [3, 4, 5],
+    'Score__evid_strength': [0.05, 0.1, 0.2],
+    'Score__kl_strength': [0.01, 0.05, 0.1],
+    }      
     
-   
+    
+    # BEST ONES: 600, 0.1 and for regs 0.1 and 0.001
     NGB_Hparams = {
         'Dist': NormalInverseGamma,
         'Score' : NIGLogScore,
         'n_estimators': 600,
         'learning_rate': 0.1,
         'natural_gradient': True,
-        #'minibatch_frac': 0.1,
+        #'Score_kwargs': {'evid_strength': 0.1, 'kl_strength': 0.01},
         'verbose': False,
         'Base': DecisionTreeRegressor(max_depth=3)  # specify the depth here
     }
@@ -81,7 +86,7 @@ def main(folder_path, data_path, target, identifier, out, folds=10):
         param_grid_ngb)
     metrics = model.evaluate(
         folds=folds, 
-        tune=True, 
+        tune=False, 
         nested=True, 
         tune_folds=20, 
         get_shap=True,
@@ -94,8 +99,8 @@ def main(folder_path, data_path, target, identifier, out, folds=10):
     logging.info(f"Aleatoric Uncertainty: {metrics['aleatoric']}")
     logging.info(f"Epistemic Uncertainty: {metrics['epistemic']}")
     model.plot(f"Actual vs. Prediction (NGBoost) - {identifier}")
-    _,_, removals= model.feature_ablation()
-    #model.calibration_analysis()
+    _,_, removals= model.feature_ablation(folds=folds, tune=True, tune_folds=-1)
+    model.calibration_analysis()
     
     
     #summands = [0, 0, 1, 0]
@@ -120,10 +125,5 @@ if __name__ == "__main__":
     #folder_path = "/Users/georgtirpitz/Library/CloudStorage/OneDrive-Persönlich/Neuromodulation/PD-MultiModal-Prediction/"
     folder_path = "/home/georg-tirpitz/Documents/PD-MultiModal-Prediction/"
     #folder_path = "/home/georg/Documents/Neuromodulation/PD-MultiModal-Prediction/"
-    #main(folder_path, "data/BDI/level1/bdi_df.csv", "diff", "BDI", "results/level1/NGBoost", 20)
-    main(folder_path, "data/BDI/level1/bdi_df.csv", "ratio", "BDI", "results/level1/NGBoost", 20)
-    #main(folder_path, "data/BDI/level2/bdi_df.csv", "diff", "BDI", "results/level2/NGBoost", 20)
-    #main(folder_path, "data/BDI/level2/bdi_df.csv", "ratio", "BDI", "results/level2/NGBoost", 20)
-    #main(folder_path, "data/BDI/level3/bdi_df.csv", "diff", "BDI", "results/level3/NGBoost", 20)
-    #main(folder_path, "data/BDI/level3/bdi_df.csv", "ratio", "BDI", "results/level3/NGBoost", 20)
+    main(folder_path, "data/BDI/level2/bdi_df.csv", "ratio", "BDI", "results/level2_test/NGBoost", -1)
     
