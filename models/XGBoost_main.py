61eece10731fd6a08f5197e37e9cd8ffb0d83cd8
Georg Tirpitz <geotir@gmx.de>
2025-07-17 17:21:23 +0200

    fixed bug with XGB tuning

--- models/XGBoost_main.py
+++ models/XGBoost_main.py
@@ -2,18 +2,13 @@ import warnings
 warnings.filterwarnings("ignore")
 import os
 os.environ["PYTHONWARNINGS"] = "ignore"
-from models.model_classes.RegressionsModels import XGBoostRegressionModel
+from model_classes.XGBoostRegressionModel import XGBoostRegressionModel
 import pandas as pd
-from faster_evidential_boost import NormalInverseGamma, NIGLogScore
-from ngboost.distns.normal import Normal, NormalCRPScore, NormalLogScore
-from sklearn.tree import DecisionTreeRegressor
-from matplotlib import pyplot as plt
-import numpy as np
-import logging
-from xgboost import XGBRegressor
-from sklearn.datasets import load_diabetes
 from sklearn.preprocessing import StandardScaler
-
+# Logging
+from utils.my_logging import Logging
+from utils.messages import Messages
+from pprint import pformat
 
 
 #from sklearn.datasets import load_diabetes
@@ -23,28 +18,46 @@ from sklearn.preprocessing import StandardScaler
 def main(folder_path, data_path, target, identifier, out, folds=10):
     test_split_size = 0.2
     Feature_Selection = {}
-    #target_col = identifier + "_" + target
-    #possible_targets = ["ratio", "diff"] 
-    #ignored_targets = [t for t in possible_targets if t != target]
-    #ignored_target_cols = [identifier + "_" + t for t in ignored_targets]
-    #data_df = pd.read_csv(folder_path + data_path)
-    #columns_to_drop = ['Pat_ID'] + [col for col in ignored_target_cols if col in data_df.columns]
-    #data_df = data_df.drop(columns=columns_to_drop)
-    #Feature_Selection['target'] = target_col
-    #Feature_Selection['features'] = [col for col in data_df.columns if col != Feature_Selection['target']]
-    #safe_path = os.path.join(folder_path, out)
+    target_col = identifier + "_" + target
+    possible_targets = ["ratio", "diff"] 
+    ignored_targets = [t for t in possible_targets if t != target]
+    ignored_target_cols = [identifier + "_" + t for t in ignored_targets]
+    data_df = pd.read_csv(folder_path + data_path)
+    trend = data_df["TimeSinceSurgery"] * 1
+    data_df[identifier + "_diff"] = data_df[target_col] + trend
+    data_df[identifier +"_ratio"] = (data_df[identifier +"_diff"] / (data_df[identifier +"_sum_pre"]* 2 + data_df[identifier +"_diff"])) * 10
+    columns_to_drop = ['Pat_ID'] + [col for col in ignored_target_cols if col in data_df.columns]
+    data_df = data_df.drop(columns=columns_to_drop)
+    data_df.drop("TimeSinceSurgery", axis=1, inplace=True)
+    Feature_Selection['target'] = target_col
+    Feature_Selection['features'] = [col for col in data_df.columns if col != Feature_Selection['target']]
+    safe_path = os.path.join(folder_path, out)
 
     ### test
-    X, y = load_diabetes(return_X_y=True, as_frame=True)
-    X = X.sample(n=150, random_state=42)
-    y = y.loc[X.index]
-    std = y.std()
+    #X, y = load_diabetes(return_X_y=True, as_frame=True)
+    #X = X.sample(n=150, random_state=42)
+    #y = y.loc[X.index]
+    #std = y.std()
     #y = (y - y.mean()) / std  # Standardize the target variable
-    data_df = pd.concat([X, y.rename("target")], axis=1)
-    Feature_Selection['target'] = "target"
-    Feature_Selection['features'] = [col for col in data_df.columns if col != Feature_Selection['target']]
-    safe_path = os.path.join(folder_path, "test/test_diabetes/NGBoost")
+    #data_df = pd.concat([X, y.rename("target")], axis=1)
+    #Feature_Selection['target'] = "target"
+    #Feature_Selection['features'] = [col for col in data_df.columns if col != Feature_Selection['target']]
+    #safe_path = os.path.join(folder_path, "test/test_diabetes/NGBoost")
     ### test ende
+    # --- SETUP LOGGING ---
+    os.makedirs(safe_path, exist_ok=True)
+    os.makedirs(os.path.join(safe_path, 'log'), exist_ok=True)
+    logging = Logging(f'{safe_path}/log').get_logger()
+
+    # --- Print all selected parameters ---
+    logging.info('-------------------------------------------')
+    logging.info(f"Folder Path: {folder_path}")
+    logging.info(f"Data Path: {data_path}")
+    logging.info(f"Target: {target}")
+    logging.info(f"Identifier: {identifier}")
+    logging.info(f"Output Path: {out}")
+    logging.info(f"Folds: {folds}")
+    logging.info('-------------------------------------------\n')
 
     
     if not os.path.exists(safe_path):
@@ -62,25 +75,28 @@ def main(folder_path, data_path, target, identifier, out, folds=10):
     
 
     # XGBoost hyperparameter grid
+    # XGBoost hyperparameter grid
     param_grid_xgb = {
-        'n_estimators': [100, 200, 300, 500],
-        'learning_rate': [0.01, 0.05, 0.1],
-        'max_depth': [3, 4, 5],
-        'subsample': [0.7, 0.9, 1.0],
-        'colsample_bytree': [0.6, 0.8, 1.0],
-        'reg_alpha': [0, 0.1, 0.5],
-        'reg_lambda': [1, 2, 5]
-    }
+    'n_estimators': [100, 200, 300],
+    'learning_rate': [0.05, 0.1],
+    'max_depth': [3, 4, 5],
+    'subsample': [0.9],
+    'colsample_bytree': [0.8],
+    'reg_alpha': [0.1, 0.5, 0.7],
+    'reg_lambda': [2, 5, 8],
+    'random_state': [42],
+    'verbosity': [0]
+}
 
     # Default XGBoost hyperparameters
     XGB_Hparams = {
-        'n_estimators': 200,
-        'learning_rate': 0.05,
+        'n_estimators': 100,
+        'learning_rate': 0.01,
         'max_depth': 4,
         'subsample': 0.9,
         'colsample_bytree': 0.8,
-        'reg_alpha': 0.1,
-        'reg_lambda': 2,
+        'reg_alpha': 0,
+        'reg_lambda': 0,
         'random_state': 42,
         'verbosity': 0
     }
@@ -94,24 +110,22 @@ def main(folder_path, data_path, target, identifier, out, folds=10):
         safe_path, 
         identifier,
         -1,
-        param_grid_xgb)
+        param_grid_xgb,
+        logging=logging)
     metrics = model.evaluate(
         folds=folds, 
-        tune=False, 
+        tune=True, 
         nested=True, 
         tune_folds=20, 
         get_shap=True,
         uncertainty=False)
     
-    # Set up logging
-    logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
-    
     # Log the metrics
     logging.info(f"Aleatoric Uncertainty: {metrics['aleatoric']}")
     logging.info(f"Epistemic Uncertainty: {metrics['epistemic']}")
     model.plot(f"Actual vs. Prediction (NGBoost) - {identifier}")
-    _,_, removals= model.feature_ablation(folds=folds, tune=False, tune_folds=-1)
-    model.calibration_analysis()
+    _,_, removals= model.feature_ablation(folds=folds, tune=True, tune_folds=20)
+    #model.calibration_analysis()
     
     
     #summands = [0, 0, 1, 0]
@@ -134,7 +148,8 @@ def main(folder_path, data_path, target, identifier, out, folds=10):
 
 if __name__ == "__main__":
     #folder_path = "/Users/georgtirpitz/Library/CloudStorage/OneDrive-Persönlich/Neuromodulation/PD-MultiModal-Prediction/"
-    folder_path = "/home/georg-tirpitz/Documents/PD-MultiModal-Prediction/"
+    #folder_path = "/home/georg-tirpitz/Documents/PD-MultiModal-Prediction/"
     #folder_path = "/home/georg/Documents/Neuromodulation/PD-MultiModal-Prediction/"
-    main(folder_path, "data/BDI/level2/bdi_df.csv", "diff", "BDI", "results/level2_test/NGBoost", 20)
-    
+    folder_path = "/home/ubuntu/PD-MultiModal-Prediction/"
+    #main(folder_path, "data/BDI/level2/bdi_df.csv", "diff", "BDI", "results/BDI_test/level2/XGBoost", 20)
+    main(folder_path, "data/MoCA/level2/moca_df.csv", "ratio", "MoCA", "results/MoCA_ratio/level2/XGBoost", 20)
