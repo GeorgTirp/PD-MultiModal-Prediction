b87c33200190058283814f9a98254d34a91c746d
Georg Tirpitz <geotir@gmx.de>
2025-07-24 18:14:47 +0200

    added code to find the outliers

--- shap_analysis.py
+++ shap_analysis.py
@@ -3,6 +3,7 @@ import shap
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
+import os
 
 def filter_shaps(data_path, shap_path, save_path, identifier, threshold=0.0, features=None):
     # Load the SHAP values from the .npy file
@@ -28,22 +29,12 @@ def filter_shaps(data_path, shap_path, save_path, identifier, threshold=0.0, fea
     plt.close()
     return data, shap_values
 
-def filter_shap_outliers(data_path, feature, shap_path, save_path, identifier, threshold=0.0, features=None):
+def filter_shap_outliers(data_path, shap_path, feature_index, save_path, identifier, threshold=0.0):
     # Load the SHAP values from the .npy file
-    shap_values = np.load(shap_path)
+    shap_values = np.mean(np.load(shap_path), axis=0)
     
     # Load the data as a pandas DataFrame
     data = pd.read_csv(data_path)
-
-    ### Do work here
-    # 0. Check if len(features) == shap_values.shape[1]
-    if features is not None and len(features) != shap_values.shape[1]:
-        raise ValueError("Length of features list does not match number of SHAP columns.")
-
-    # 1. Find index of feature
-    if feature not in features:
-        raise ValueError(f"Feature '{feature}' not found in feature list.")
-    feature_index = features.index(feature)
 
     # 2. Get SHAP values for that column
     shap_col_values = shap_values[:, feature_index]
@@ -58,15 +49,8 @@ def filter_shap_outliers(data_path, feature, shap_path, save_path, identifier, t
     # Optional: create output directory
     os.makedirs(save_path, exist_ok=True)
 
-    # Replot the beeswarm plot using the filtered SHAP values
-    shap.summary_plot(filtered_shap_values, filtered_data, features, show=False)
-    plt.title(f'Filtered Summary Plot (Aggregated - Mean)', fontsize=16)
-    plt.subplots_adjust(top=0.90)
-    plt.savefig(f'{save_path}/{identifier}_filter_geq_{threshold}.png')
-    plt.close()
-
     filtered_csv_path = os.path.join(save_path, f'{identifier}_filtered_outliers_{threshold}.csv')
-    filtered_data.to_csv(filtered_csv_path, index=False)
+    filtered_data.to_csv(filtered_csv_path)
 
     return filtered_data, filtered_shap_values
 
@@ -88,36 +72,27 @@ def covariance_matrix(data_path, safe_path: str = "") -> pd.DataFrame:
 
 if __name__ == "__main__":
     # Example usage
-    data_path1 = "/home/georg-tirpitz/Documents/PD-MultiModal-Prediction/data/BDI/level2/bdi_df.csv"
-    shap_path1 = "/home/georg-tirpitz/Documents/PD-MultiModal-Prediction/results/level2/NGBoost/BDI_mean_shap_values.npy"
+    #data_path1 = "/home/georg-tirpitz/Documents/PD-MultiModal-Prediction/data/BDI/level2/bdi_df.csv"
+    #shap_path1 = "/home/georg-tirpitz/Documents/PD-MultiModal-Prediction/results/level2/NGBoost/BDI_mean_shap_values.npy"
 
-    data_path2 = "/home/ubuntu/PD-MultiModal-Prediction/data/MocA/level2/moca_NGBoost_detrend_filter_df.csv"
-    shap_path2 = "/home/ubuntu/PD-MultiModal-Prediction/results/MoCA_NGBoost_diff/level2/NGBoost/ablation/ablation_step[3]/MoCA_diff_all_shap_values(mu).npy"
+    data_path = "/home/ubuntu/PD-MultiModal-Prediction/data/MoCA/level2/moca_NGBoost_detrend_filter_df.csv"
+    shap_path = "/home/ubuntu/PD-MultiModal-Prediction/results/MoCA_NGBoost_diff/level2/NGBoost/ablation/ablation_step[3]/MoCA_diff_all_shap_values(mu).npy"
 
-    save_path = "//home/ubuntu/PD-MultiModal-Prediction/results/"
+    save_path = "/home/ubuntu/PD-MultiModal-Prediction/results/"
     # Load the data and SHAP values
     #data, shap_values = filter_shaps(data_path2, shap_path2, save_path, "level2", threshold=3.0)
-    features = [
-        "AGE_AT_OP",
-        "Left_1_mA",
-        "TimeSinceDiag",
-        "LEDD_reduc",
-        "L_distance",
-        "MoCA_sum_pre",
-    ]
-    data, shap_values = filter_shap_outliers(
-        data_path2, 
-        shap_path2, 
-        "MoCA_sum_pre",
-        save_path, 
-        "level2_9_features", 
-        threshold=3.0,
-        features=features)
     
-
+    data, shap_values = filter_shap_outliers(
+        data_path=data_path, 
+        shap_path=shap_path, 
+        feature_index=2,
+        save_path=save_path, 
+        identifier="level2_9_features", 
+        threshold=3.0)
+    
     #covariance_matrix(data_path2, "results/level2")
     #covariance_matrix(data_path3, "results/level3") 
     
     # Print the shapes of the loaded data and SHAP values
-    print("Data shape:", data.shape)
-    print("SHAP values shape:", shap_values.shape)
+    #print("Data shape:", data.shape)
+    #print("SHAP values shape:", shap_values.shape)
